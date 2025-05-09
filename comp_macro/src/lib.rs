3bcadd17ae91d2594bb67d3c22ecf984c0484303
Ayhan Eyikan <aeyikan@umd.edu>
2025-05-09 12:28:08 -0400

    Init intermediate representation

--- comp_macro/src/lib.rs
+++ comp_macro/src/lib.rs
@@ -6,3 +6,22 @@
 //
 // pattern: name (, name)*
 
+struct Comp {
+    mapping: Mapping,
+    for_if_clause: ForIfClause,
+}
+
+// Using `syn` crate for representing Rust types
+struct Mapping(syn::Expr);
+
+struct ForIfClause {
+    pattern: Pattern,
+    expr: syn::Expr,
+    // optional: zero or more conditions
+    condition: Vec<Condition>,
+}
+
+// Using `syn`'s representation of Rust patterns
+struct Pattern(syn::Pat);
+
+struct Condition(syn::Expr);
